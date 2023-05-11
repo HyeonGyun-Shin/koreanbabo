c658b439df12d42e53f2c59ad2bf49cc1b4c97f7
EC2 Default User <ec2-user@ip-10-0-1-184.ap-northeast-2.compute.internal>
2023-05-11 09:29:33 +0000

    hamster Hello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This CI/CD Test Success!';
+    return 'This CI/CD Test Success!!!';
   }
 }
