c4449934a9bcd6868cf91c1a2908917d645293ec
HyeonGyun-Shin <64944854+HyeonGyun-Shin@users.noreply.github.com>
2023-05-11 15:56:32 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ -d "/home/ec2-user/koreanbabo/dist" ]; then rm -Rf "/home/ec2-user/koreanbabo/dist"; fi
-if [ -f "/home/ec2-user/koreanbabo/appspec.yml" ]; then rm -Rf "/home/ec2-user/koreanbabo/appspec.yml"; fi
-if [ -d "/home/ec2-user/koreanbabo/scripts" ]; then rm -Rf "/home/ec2-user/koreanbabo/scripts"; fi
+if [ -d "/home/ec2-user/koreanbabo/dist" ]; then sudo rm -Rf "/home/ec2-user/koreanbabo/dist"; fi
+if [ -f "/home/ec2-user/koreanbabo/appspec.yml" ]; then sudo rm -Rf "/home/ec2-user/koreanbabo/appspec.yml"; fi
+if [ -d "/home/ec2-user/koreanbabo/scripts" ]; then sudo rm -Rf "/home/ec2-user/koreanbabo/scripts"; fi
