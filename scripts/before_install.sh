cdd88e5d680e64b950070a01864ae4f347955da8
EC2 Default User <ec2-user@ip-10-0-1-107.ap-northeast-2.compute.internal>
2023-05-16 08:03:14 +0000

    korean babo 3

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-if [ -d "/home/ec2-user/koreanbabo/dist" ]; then sudo rm -Rf "/home/ec2-user/koreanbabo/dist"; fi
-if [ -f "/home/ec2-user/koreanbabo/appspec.yml" ]; then sudo rm -Rf "/home/ec2-user/koreanbabo/appspec.yml"; fi
-if [ -d "/home/ec2-user/koreanbabo/scripts" ]; then sudo rm -Rf "/home/ec2-user/koreanbabo/scripts"; fi
+npm install yarn -g
+yarn global add pm2
