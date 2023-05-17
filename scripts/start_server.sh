02e5eff079f941b98176d13e47ecab220f2736d4
EC2 Default User <ec2-user@ip-10-0-1-231.ap-northeast-2.compute.internal>
2023-05-17 01:42:00 +0000

    test 1

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16
-npm install yarn -g
-yarn global add pm2
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /home/ec2-user/koreanbabo
-pwd
 sudo chown ec2-user:ec2-user appspec.yml build-test.zip scripts/*
 yarn pm2 kill
 yarn pm2 start dist/main.js
