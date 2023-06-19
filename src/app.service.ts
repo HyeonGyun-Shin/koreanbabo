5396bd994383761b8bacf8115490119803a92bdd
HyeonGyun-Shin <hyeongyun32@gmail.com>
2023-06-19 14:05:27 +0900

    korean babo

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is Node CI/CD Success! korean babo test';
+    return 'This is AutoScaling CodeSeries Test!!! KOREAN BABO';
   }
 }
